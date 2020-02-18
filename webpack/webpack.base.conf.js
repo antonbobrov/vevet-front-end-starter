7c0da89288ae040c9c1b1745479c67dc31fd9d09
Anton Bobrov <anton.bobrov@hotmail.com>
2020-02-18 18:37:58 +0300

    GLSL support

--- webpack/webpack.base.conf.js
+++ webpack/webpack.base.conf.js
@@ -112,6 +112,14 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                exclude: /node_modules/,
+                use: [
+                    'raw-loader',
+                    'glslify-loader'
+                ]
             }
         ]
 
