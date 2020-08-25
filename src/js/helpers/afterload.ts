9de33a4d74d82b80337b616f7d7459812060c90c
Anton Bobrov <anton.bobrov@hotmail.com>
2020-08-26 00:57:54 +0300

    Update dependencies

--- src/js/helpers/afterload.ts
+++ src/js/helpers/afterload.ts
@@ -1,20 +1,22 @@
-import { afterloadDuration } from "../settings";
+import { selectAll } from 'vevet-dom';
+import { afterloadDuration } from '../settings';
 
 // show elements when the page is loaded
 // see also /src/styles/helpers/_afterload.scss
 
-
 function afterload () {
 
-    const el = document.querySelectorAll(".afterload");
-    for (let i = 0; i < el.length; i++) {
-        el[i].classList.add("afterload_show");
+    const el = selectAll('.afterload');
+    for (let i = 0, l = el.length; i < l; i++) {
+        const e = el[i];
+        e.classList.add('afterload_show');
     }
 
     setTimeout(() => {
-        for (let i = 0; i < el.length; i++) {
-            el[i].classList.remove("afterload");
-            el[i].classList.remove("afterload_show");
+        for (let i = 0, l = el.length; i < l; i++) {
+            const e = el[i];
+            e.classList.remove('afterload');
+            e.classList.remove('afterload_show');
         }
     }, (afterloadDuration + 50));
 
