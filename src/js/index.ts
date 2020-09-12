9ba5f72ba5fef46e3e5873d0bf55a6981bce4f1d
Anton Bobrov <anton.bobrov@hotmail.com>
2020-09-12 23:30:38 +0300

    Fix mobile scroll bug

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,15 @@
 import '../styles/index.scss';
 import { initSite } from './initSite';
 import { registerServiceWorker } from './service-worker';
+import app from './v/app';
 
 initSite();
 registerServiceWorker();
+
+// scroll to top on resize (mobile bug)
+app.viewport.on('', () => {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+}, {
+    timeout: 650,
+});
