1f2246be777444d84cf8798cda482510c7e494ca
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-04-07 22:31:18 +0300

    Add OnPageCreated helper

---
+++ src/js/app/OnPageCreated.ts
@@ -0,0 +1,36 @@
+import app from './app';
+
+export class OnPageCreated {
+
+    protected _proceeded = false;
+    protected _timeout: false | NodeJS.Timeout = false;
+
+    constructor (
+        protected _callback: () => void,
+    ) {
+        this._trace();
+    }
+
+    protected _trace () {
+        if (this._proceeded) {
+            return;
+        }
+        const page = app.vevetPage;
+        if (page) {
+            this._proceeded = true;
+            this._callback();
+        }
+        else {
+            this._timeout = setTimeout(() => {
+                this._trace();
+            }, 50);
+        }
+    }
+
+    public destroy () {
+        if (this._timeout) {
+            clearTimeout(this._timeout);
+        }
+    }
+
+}
