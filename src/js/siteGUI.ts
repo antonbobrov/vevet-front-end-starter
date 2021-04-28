8b6e0587d05cbb87739b44932fb16877e7ef5adf
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-04-28 17:15:55 +0300

    Update dat.gui

--- src/js/siteGUI.ts
+++ src/js/siteGUI.ts
@@ -1,22 +1,23 @@
 import dat, { GUI } from 'dat.gui';
-import { KeydownModule } from 'vevet';
 import app from './app/app';
+
+
+
+export const minSiteGUIStep = 0.0001;
+
+
 
 export const siteGUI: false | dat.GUI = (function () {
 
+    if (app.viewport.mobiledevice) {
+        return false;
+    }
+    if (!app.html.classList.contains('has-gui')) {
+        return false;
+    }
+
     const gui = new dat.GUI();
     gui.close();
-
-    // show on keyboard "gui" type
-    app.html.classList.add('show-dat-gui');
-    window.addEventListener('load', () => {
-        const keyboardEvent = new KeydownModule();
-        keyboardEvent.on('queue', () => {
-            app.html.classList.add('show-dat-gui');
-        }, {
-            keys: [71, 85, 73],
-        });
-    });
 
     return gui;
 
