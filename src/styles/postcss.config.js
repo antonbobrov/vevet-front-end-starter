e30422c98f9c70784b5426b720c77524afae7368
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-04-28 17:39:50 +0300

    Disable color minification

--- src/styles/postcss.config.js
+++ src/styles/postcss.config.js
@@ -10,6 +10,7 @@ module.exports = {
                     discardComments: {
                         removeAll: true,
                     },
+                    colormin: false,
                 },
             ],
         }),
