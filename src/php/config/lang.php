f4e18a1ed02878031aa226aab23b959edcd24e85
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-04-28 17:42:07 +0300

    Update sql query

--- src/php/config/lang.php
+++ src/php/config/lang.php
@@ -15,7 +15,26 @@ if (isset($_GET['contexts'])) {
     foreach ($lang as $key => $settings) {
         foreach($settings as $keySettings => $settingsSettings){
             for($i = 0; $i < count($contexts); $i++){
-                echo "INSERT INTO `modx_context_setting` (`context_key`, `key`, `value`, `xtype`, `namespace`, `area`, `editedon`) VALUES ('".$contexts[$i]."', '".$key."_".$keySettings."', '".$settingsSettings."', 'textfield', 'core', 'language', NULL);";
+                echo "
+                    INSERT IGNORE INTO `modx_context_setting` 
+                    (
+                        `context_key`, 
+                        `key`, 
+                        `value`, 
+                        `xtype`, 
+                        `namespace`, 
+                        `area`, 
+                        `editedon`
+                    ) 
+                    VALUES (
+                        '".$contexts[$i]."', 
+                        '".$key."_".$keySettings."', 
+                        '".$settingsSettings."', 
+                        'textfield', 'core', 
+                        'language', 
+                        NULL
+                    );
+                ";
             }
         }
     }
