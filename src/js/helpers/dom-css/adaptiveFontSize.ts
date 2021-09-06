8a89d9ef825b7c0f87ccc2cf9b6372a3581fb8e3
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-09-06 11:40:27 +0300

    Update adaptiveFontSize

--- src/js/helpers/dom-css/adaptiveFontSize.ts
+++ src/js/helpers/dom-css/adaptiveFontSize.ts
@@ -6,116 +6,113 @@ import { boundProgress } from '../math/boundProgress';
 
 
 
-interface Return {
-    set: Function;
-    getValue: () => number;
-}
-
-export const adaptiveFontSize = (function fontSize (): Return {
-
+const adaptiveFontSize = (function func () {
     let prevValue = 16;
 
-
-
-    // get font size
-    function getValue () {
-
-        if (!useAdaptiveFontSize) {
-            return 16;
-        }
-
-        const { viewport } = app;
-        const [width, height] = viewport.size;
-        let multiplier = 1;
-
-        // dekstop
-        if (viewport.desktop) {
-            if (width < 1440) {
-                multiplier = width / 1440;
-            }
-            else if (width >= 1440 && width <= 1580) {
-                multiplier = 1;
-            }
-            else {
-                multiplier = width / 1580;
-            }
-        }
-        // tablet
-        else if (viewport.tablet) {
-            multiplier = width / 1024;
-        }
-        // mobile
-        else if (viewport.mobile) {
-            if (app.viewport.landscape) {
-                multiplier = 1;
-            }
-            else if (width > 750) {
-                multiplier = width / 500;
-            }
-            else if (width > height) {
-                if (width >= 360 && width <= 400) {
-                    multiplier = 1;
-                }
-                else if (width < 360) {
-                    multiplier = boundProgress(width / 360, [0.9375, Infinity]);
-                }
-                else if (width > 400) {
-                    multiplier = width / width;
-                }
-            }
-            else {
-                multiplier = 1;
-            }
-        }
-
-        // calculate
+    /**
+     * Get current value
+     */
+    function get () {
+        const multiplier = getMultiplier();
+        // calculate font size
         const font = Math.round(multiplier * 16);
         return boundProgress(font, [13, 27]);
+    }
 
+    /**
+     * Get font size multiplier
+     */
+    function getMultiplier () {
+        if (!app || !useAdaptiveFontSize) {
+            return 1;
+        }
+        const { viewport } = app;
+        const {
+            desktop, tablet, mobile,
+            landscape, size,
+        } = viewport;
+        const [width, height] = size;
+
+        if (desktop) {
+            if (width < 1440) {
+                return width / 1440;
+            }
+            if (width >= 1440 && width <= 1580) {
+                return 1;
+            }
+            return width / 1580;
+        }
+
+        if (tablet) {
+            return width / 1024;
+        }
+
+        if (mobile) {
+            if (landscape) {
+                return 1;
+            }
+            if (width > 750) {
+                return width / 500;
+            }
+            if (width > height) {
+                if (width >= 360 && width <= 400) {
+                    return 1;
+                }
+                if (width < 360) {
+                    return boundProgress(width / 360, [0.9375, Infinity]);
+                }
+                if (width > 400) {
+                    return width / width;
+                }
+            }
+            else if (width < 350) {
+                return width / 350;
+            }
+            else {
+                return 1;
+            }
+        }
+
+        return 1;
     }
 
 
 
-    // set font size
-    function set () {
-        const value = getValue();
+    /**
+     * Update font size
+     */
+    function update () {
+        if (!app) {
+            return;
+        }
+        const value = get();
         if (value !== prevValue) {
             app.html.style.fontSize = `${value}px`;
+            app.html.style.setProperty('--font-size', `${value}px`);
         }
         prevValue = value;
     }
-    set();
 
-
-
-    // add viewport callbacks
-    app.viewport.add({
-        target: 'w_',
-        do: set.bind(this),
-        name: 'Font Size',
-    });
-
-    app.load.add({
-        do: set.bind(this),
-    });
-
-
+    // set events
+    update();
+    if (app) {
+        app.viewport.on('w', () => {
+            update();
+        }, {
+            name: 'Adaptive Font Size',
+        });
+    }
 
     return {
-        set: set.bind(this),
-        getValue: getValue.bind(this),
+        get,
+        px: (
+            val: number,
+            round = false,
+        ) => {
+            const px = val * (get() / 16);
+            return round ? Math.round(px) : px;
+        },
     };
-
-
-
 }());
 
-
-
-export function adaptivePx (
-    val: number,
-    round = false,
-) {
-    const px = val * adaptiveFontSize.getValue() / 16;
-    return round ? Math.round(px) : px;
-}
+export default adaptiveFontSize;
